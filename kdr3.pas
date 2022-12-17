b4bb8b48366d4309f71cb07e7828de4e082bb64f
VovaWeepy <117993619+VovaWeepy@users.noreply.github.com>
2022-12-17 13:00:37 +0300

    Add files via upload

---
+++ kdr3.pas
@@ -0,0 +1,31 @@
+﻿uses crt;
+var
+  i,a, b, n: integer;
+  h,x, s, p, pogr: real;
+
+function f(x: real): real;
+begin
+  f := (2 * power(x, 3)) + ((1) * power(x, 2)) + ((0) * x + (9));
+end;
+
+function m(x: real): real;
+begin
+  m := (2*x * (power(x,4)/4) + ((-2)*x) * (power(x,3)/3) + (3*x*x)/2 + 8 * x);
+end;
+begin
+  readln(a, b, n);
+  h := (b - a) / n;
+  x := a;
+  for i := 0 to n - 1 do 
+  begin
+    s += f(x);
+    x += h;
+  end;
+  s *= h;
+  writeln(s);
+  p := m(b) - m(a);
+  pogr := s - p;
+  writeln(p);  
+  writeln(pogr);
+end.
+
